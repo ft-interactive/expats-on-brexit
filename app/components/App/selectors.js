2472418a55842d4a440e8a6528b53a0451638d1c
Callum Locke <callum.locke@gmail.com>
2016-05-19 01:28:12 +0100

    add dummy data

--- app/components/App/selectors.js
+++ app/components/App/selectors.js
@@ -1,3 +1,5 @@
+import { createSelector } from 'reselect';
+
 // selectLocationState expects a plain JS object for the routing state
 export const selectLocationState = () => {
   let prevRoutingState;
@@ -12,3 +14,10 @@ export const selectLocationState = () => {
     return prevRoutingState;
   };
 };
+
+export const selectAppState = state => {
+  console.log('state', state);
+  return state.app;
+};
+
+export const selectOptions = createSelector(selectAppState, appState => appState.options);
--- app/components/HomePage/index.js
+++ app/components/HomePage/index.js
@@ -1,21 +1,23 @@
-/*
- * HomePage
- *
- * This is the first thing users see of our App, at the '/' route
- */
-
-import React from 'react';
+import React, { PropTypes } from 'react';
 import InternalLink from '../InternalLink';
 import OFooter from '../OFooter';
 import styles from './styles.scss';
+import { connect } from 'react-redux';
+import { createSelector, createStructuredSelector } from 'reselect';
+import { selectOptions } from '../App/selectors';
 
-export function HomePage() {
+export function HomePage({ headline, standfirst }) {
   return (
     <div className={styles.homePage}>
       <div className={styles.mainContent}>
-        <h1>This is the Homepage!</h1>
+        <h1>{headline}</h1>
+        <p>{standfirst}</p>
 
         <InternalLink route="/form" className={styles.floatingActionButton}>Write a comment</InternalLink>
+
+        <section className={styles.opinionsList}>
+          {[...Array(200).keys()].map((x, i) => <p key={i} style={{ background:'#efe' }}>opinions</p>)}
+        </section>
       </div>
 
       <OFooter />
@@ -23,4 +25,14 @@ export function HomePage() {
   );
 }
 
-export default HomePage;
+HomePage.propTypes = {
+  headline: PropTypes.string.isRequired,
+  standfirst: PropTypes.string.isRequired,
+};
+
+const select = createStructuredSelector({
+  headline: createSelector(selectOptions, options => options.headline),
+  standfirst: createSelector(selectOptions, options => options.standfirst),
+});
+
+export default connect(select)(HomePage);
