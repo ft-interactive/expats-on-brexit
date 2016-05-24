f4b2d0b5820a03ef520807f9689ceedc1b54e032
Callum Locke <callum.locke@gmail.com>
2016-05-24 21:18:17 +0100

    landing page stuff, byline links

--- app/components/LandingPage/index.js
+++ app/components/LandingPage/index.js
@@ -20,6 +20,10 @@ function LandingPage({ opinions }) {
     />
   );
 
+  // const bylines = {
+  //   'Lilah&nbsp;Raptopoulos': 
+  // };
+
   return (
     <div className="landing-page page">
       <SiteHeader simple />
@@ -40,8 +44,9 @@ function LandingPage({ opinions }) {
           </p>
 
           <div className="landing-page__header-meta">
+
             <p className="landing-page__byline">
-              By Lilah&nbsp;Raptopoulos, Callum&nbsp;Locke, Caroline&nbsp;Nevitt and Aleks&nbsp;Wisniewska
+              By <a href="https://twitter.com/lilahrap">Lilah&nbsp;Raptopoulos</a>, <a href="https://twitter.com/callumlocke">Callum&nbsp;Locke</a>, <a href="https://twitter.com/carolinenevitt">Caroline&nbsp;Nevitt</a> and <a href="https://twitter.com/alekswis">Aleks&nbsp;Wisniewska</a>
             </p>
 
             <p className="landing-page__date">
@@ -51,7 +56,7 @@ function LandingPage({ opinions }) {
         </header>
 
         <div className="landing-page__copy">
-          <p>Millions of Britons living abroad have an interest in the outcome of the EU referendum, and many of them can vote. With the June 7 voting registration deadline looming, we asked those FT readers to describe how the outcome would affect them and what living abroad has taught them. Responses flooded in from Malta to Ghana to South Korea. Click the button below to explore their points of view.</p>
+          <p>Millions of Britons living abroad have an interest in the outcome of the <a href="https://www.ft.com/eu-referendum">EU referendum</a>, and many of them can vote. With the June 7 voting registration deadline looming, we asked those FT readers to describe how the outcome would affect them and what living abroad has taught them. Responses flooded in from Malta to Ghana to South Korea. Click the button below to explore their points of view.</p>
 
           <p className="landing-page__explore-link">
             <InternalLink route="/explore" className="btn btn--dark btn--raised">
@@ -63,6 +68,24 @@ function LandingPage({ opinions }) {
           <p>An estimated 5m British citizens live outside the UK, and they lean overwhelmingly in favour of remaining in the EU. Independent research conducted by global expat network Angloinfo put the split at 73 per cent Remain and 20 per cent Leave, with 7 per cent undecided. Though unscientific, our results show a similar breakdown: 71 per cent of our total respondents lean towards staying in. Here are some standout perspectives.</p>
 
           <div className="landing-page__factbox-pair">
+            <aside className="landing-page__factbox">
+              <h4>FT coverage on the EU referendum</h4>
+              <ul>
+                <li>
+                  <a href="https://www.ft.com/eu-referendum">Our Brexit hub page</a>
+                </li>
+                <li>
+                  <a href="https://www.ft.com/content/e7b2d4d4-daea-11e5-98fd-06d75973fe09">Your top 10 questions answered</a>
+                </li>
+                <li>
+                  <a href="https://www.ft.com/content/1465ef50-da34-11e5-98fd-06d75973fe09">What Brexit means for the UK economy</a>
+                </li>
+                <li>
+                  <a href="https://www.ft.com/content/1a86ab36-afbe-11e5-b955-1a1d298b6250">What are economists’ views on Brexit?</a>
+                </li>
+              </ul>
+            </aside>
+
             <aside className="landing-page__factbox">
               <h4>Public information for British expats</h4>
               <ul>
@@ -85,24 +108,6 @@ function LandingPage({ opinions }) {
                   <a href="http://www.strongerin.co.uk/" target="_blank">
                     Vote Remain campaign
                   </a>
-                </li>
-              </ul>
-            </aside>
-
-            <aside className="landing-page__factbox">
-              <h4>FT coverage on the EU referendum</h4>
-              <ul>
-                <li>
-                  <a href="https://www.ft.com/eu-referendum">Our Brexit hub page</a>
-                </li>
-                <li>
-                  <a href="https://www.ft.com/content/e7b2d4d4-daea-11e5-98fd-06d75973fe09">Your top 10 questions answered</a>
-                </li>
-                <li>
-                  <a href="https://www.ft.com/content/1465ef50-da34-11e5-98fd-06d75973fe09">What Brexit means for the UK economy</a>
-                </li>
-                <li>
-                  <a href="https://www.ft.com/content/1a86ab36-afbe-11e5-b955-1a1d298b6250">What are economists’ views on Brexit?</a>
                 </li>
               </ul>
             </aside>
@@ -155,9 +160,9 @@ function LandingPage({ opinions }) {
         
 
         <div className="landing-page__invitation-well">
-          <p>Are you also a British expat? We want to hear from you.</p>
-          <div>
-            <InternalLink route="/form" className="btn btn--raised">Add your view</InternalLink>
+          <p>Are&nbsp;you&nbsp;also&nbsp;a&nbsp;British&nbsp;expat? We&nbsp;want&nbsp;to&nbsp;hear&nbsp;from&nbsp;you.</p>
+          <div className="landing-page__add-your-view-button">
+            <InternalLink route="/form" className="btn">Add your view</InternalLink>
           </div>
         </div>
 
